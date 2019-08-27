2f6c09f6ad7fb52bfc781b0dbb66ed58bd29e2c9
Shu OGAWARA <expajp@gmail.com>
2019-08-27 20:49:09 +0900

    implement my_string_inquirer

--- reimplement_string_inquirer/my_string_inquirer.rb
+++ reimplement_string_inquirer/my_string_inquirer.rb
@@ -1,2 +1,13 @@
-class MyStringInquirer
+class MyStringInquirer < String
+  def method_missing(name, *args)
+    if name.to_s.end_with?('?')
+      self == name.to_s[0..-2]
+    else
+      super
+    end
+  end
+  
+  def respond_to_missing?(name, include_private = false)
+    name.to_s.end_with?('?')
+  end
 end
